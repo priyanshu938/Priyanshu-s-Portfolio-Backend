86a31aad37247820d1ee45768bdd15700df46d92
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2022-07-31 15:06:01 +0530

    auth token fetching optimized

--- api/middleware/check_auth.js
+++ api/middleware/check_auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1]; //accesses the token from the headers.Format :- Bearer <auth-token>
+    const token = req.header("auth-token");
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY); //verify the auth token
     req.userData = decoded; // now set the userData with this token and in we can use this to verify
     next();
